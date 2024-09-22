5fb8c70cfe7c86727dc272f40bdb3c72482d1f4a
ep777 <ep777@MAIBENBEN-PC>
2024-09-23 02:30:37 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace ClassLibrary1
+{
+    public class PasswordValidator
+    {
+        public int ValidatePassword(string password)
+        {
+            int score = 0;
+
+            // Проверка на наличие цифр
+            if (Regex.IsMatch(password, @"[012345678]"))
+                score++;
+
+            // Проверка на наличие строчных букв
+            if (Regex.IsMatch(password, @"[a-z]"))
+                score++;
+
+            // Проверка на наличие заглавных букв
+            if (Regex.IsMatch(password, @"[A-Z]"))
+                score++;
+
+            // Проверка на наличие спец. символов
+            if (Regex.IsMatch(password, @"[\W_]"))
+                score++;
+
+            // Проверка на длину
+            if (password.Length > 7)
+                score++;
+
+            return score;
+        }
+    }
+}
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,79 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class Test1
+    {
+        private PasswordValidator validator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            validator = new PasswordValidator();
+        }
+
+        [TestMethod]
+        public void TestPasswordScore1()
+        {
+            string password = "Password123";
+            int result = validator.ValidatePassword(password);
+
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void TestPasswordScore2()
+        {
+            string password = "Password";
+            int result = validator.ValidatePassword(password);
+
+
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void TestPasswordScore3()
+        {
+            string password = "password";
+            int result = validator.ValidatePassword(password);
+
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestPasswordScore4()
+        {
+            string password = "Password.";
+            int result = validator.ValidatePassword(password);
+
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void TestPasswordScore5()
+        {
+            string password = "P@ssw0rd";
+            int result = validator.ValidatePassword(password);
+
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void TestPasswordScore0()
+        {
+            string password = "pass";
+            int result = validator.ValidatePassword(password);
+
+            Assert.AreEqual(result, 0);
+        }
+    }
+
+}
+
